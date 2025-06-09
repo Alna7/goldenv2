079ed9a009e2fa08de372482d0cf94ba1f092dae
Alna7 <126671566+Alna7@users.noreply.github.com>
2025-06-09 16:45:59 +0330

    Update MainActivity.java

--- app/src/main/java/dev/dev7/example/MainActivity.java
+++ app/src/main/java/dev/dev7/example/MainActivity.java
@@ -1,25 +1,16 @@
 package dev.dev7.example;
 
-import static dev.dev7.lib.v2ray.utils.V2rayConstants.SERVICE_CONNECTION_STATE_BROADCAST_EXTRA;
-import static dev.dev7.lib.v2ray.utils.V2rayConstants.SERVICE_DOWNLOAD_SPEED_BROADCAST_EXTRA;
-import static dev.dev7.lib.v2ray.utils.V2rayConstants.SERVICE_DOWNLOAD_TRAFFIC_BROADCAST_EXTRA;
-import static dev.dev7.lib.v2ray.utils.V2rayConstants.SERVICE_DURATION_BROADCAST_EXTRA;
-import static dev.dev7.lib.v2ray.utils.V2rayConstants.SERVICE_UPLOAD_SPEED_BROADCAST_EXTRA;
-import static dev.dev7.lib.v2ray.utils.V2rayConstants.SERVICE_UPLOAD_TRAFFIC_BROADCAST_EXTRA;
-import static dev.dev7.lib.v2ray.utils.V2rayConstants.V2RAY_SERVICE_STATICS_BROADCAST_INTENT;
+import static dev.dev7.lib.v2ray.utils.V2rayConstants.*;
 
 import android.annotation.SuppressLint;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.SharedPreferences;
+import android.content.*;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -32,9 +23,8 @@ import dev.dev7.lib.v2ray.utils.V2rayConstants;
 public class MainActivity extends AppCompatActivity {
 
     private Button connection;
-    private TextView connection_speed, connection_traffic, connection_time, server_delay, connected_server_delay, connection_mode,core_version;
-    private EditText v2ray_config;
-    private SharedPreferences sharedPreferences;
+    private TextView connection_speed, connection_traffic, connection_time, server_delay, connected_server_delay, connection_mode, core_version;
+    private EditText uuid_input;
     private BroadcastReceiver v2rayBroadCastReceiver;
 
     @SuppressLint({"SetTextI18n", "UnspecifiedRegisterReceiverFlag"})
@@ -42,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         if (savedInstanceState == null) {
             V2rayController.init(this, R.drawable.ic_launcher, "V2ray Android");
             connection = findViewById(R.id.btn_connection);
@@ -51,35 +42,48 @@ public class MainActivity extends AppCompatActivity {
             server_delay = findViewById(R.id.server_delay);
             connection_mode = findViewById(R.id.connection_mode);
             connected_server_delay = findViewById(R.id.connected_server_delay);
-            v2ray_config = findViewById(R.id.v2ray_config);
+            uuid_input = findViewById(R.id.uuid_input);
             core_version = findViewById(R.id.core_version);
         }
 
         core_version.setText(V2rayController.getCoreVersion());
-        // initialize shared preferences for save or reload default config
-        sharedPreferences = getSharedPreferences("conf", MODE_PRIVATE);
-        // reload previous config to edit text
-        v2ray_config.setText(sharedPreferences.getString("v2ray_config", getDefaultConfig()));
+
         connection.setOnClickListener(view -> {
-            sharedPreferences.edit().putString("v2ray_config", v2ray_config.getText().toString()).apply();
-            if (V2rayController.getConnectionState() == V2rayConstants.CONNECTION_STATES.DISCONNECTED) {
-                V2rayController.startV2ray(this, "Test Server", v2ray_config.getText().toString(), null);
+            String userUUID = uuid_input.getText().toString().trim();
+
+            if (userUUID.length() < 30 || !userUUID.matches("^[0-9a-fA-F\\-]{36}$")) {
+                Toast.makeText(this, "UUID is invalid!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            String config = "[{\\\"v\\\":\\\"2\\\",\\\"ps\\\":\\\"Turkey\\\",\\\"add\\\":\\\"185.143.234.120\\\",\\\"port\\\":\\\"443\\\",\\\"id\\\":\\\"" + userUUID + "\\\",\\\"aid\\\":\\\"0\\\",\\\"net\\\":\\\"ws\\\",\\\"type\\\":\\\"none\\\",\\\"host\\\":\\\"app.alnafun.ir\\\",\\\"path\\\":\\\"/\\\",\\\"tls\\\":\\\"tls\\\",\\\"sni\\\":\\\"iau.ac.ir\\\"}]";
+
+            if (V2rayController.getConnectionState() == CONNECTION_STATES.DISCONNECTED) {
+                V2rayController.startV2ray(this, "Turkey", config, null);
             } else {
                 V2rayController.stopV2ray(this);
             }
         });
 
-
-        // Check the connection delay of connected config.
         connected_server_delay.setOnClickListener(view -> {
             connected_server_delay.setText("connected server delay : measuring...");
-            // Don`t forget to do ui jobs in ui thread!
-            V2rayController.getConnectedV2rayServerDelay(this, delayResult -> runOnUiThread(() -> connected_server_delay.setText("connected server delay : " + delayResult + "ms")));
+            V2rayController.getConnectedV2rayServerDelay(this, delayResult -> runOnUiThread(() ->
+                    connected_server_delay.setText("connected server delay : " + delayResult + "ms")));
         });
-        // Another way to check the connection delay of a config without connecting to it.
+
         server_delay.setOnClickListener(view -> {
+            String userUUID = uuid_input.getText().toString().trim();
+
+            if (userUUID.length() < 30 || !userUUID.matches("^[0-9a-fA-F\\-]{36}$")) {
+                Toast.makeText(this, "UUID is invalid!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            String config = "[{\\\"v\\\":\\\"2\\\",\\\"ps\\\":\\\"Turkey\\\",\\\"add\\\":\\\"185.143.234.120\\\",\\\"port\\\":\\\"443\\\",\\\"id\\\":\\\"" + userUUID + "\\\",\\\"aid\\\":\\\"0\\\",\\\"net\\\":\\\"ws\\\",\\\"type\\\":\\\"none\\\",\\\"host\\\":\\\"app.alnafun.ir\\\",\\\"path\\\":\\\"/\\\",\\\"tls\\\":\\\"tls\\\",\\\"sni\\\":\\\"iau.ac.ir\\\"}]";
+
             server_delay.setText("server delay : measuring...");
-            new Handler().postDelayed(() -> server_delay.setText("server delay : " + V2rayController.getV2rayServerDelay(v2ray_config.getText().toString()) + "ms"), 200);
+            new Handler().postDelayed(() ->
+                    server_delay.setText("server delay : " + V2rayController.getV2rayServerDelay(config) + "ms"), 200);
         });
 
         connection_mode.setOnClickListener(view -> {
@@ -87,11 +91,9 @@ public class MainActivity extends AppCompatActivity {
             connection_mode.setText("connection mode : " + V2rayConfigs.serviceMode.toString());
         });
 
-        // Check connection state when activity launch
         switch (V2rayController.getConnectionState()) {
             case CONNECTED:
                 connection.setText("CONNECTED");
-                // check  connection latency
                 connected_server_delay.callOnClick();
                 break;
             case DISCONNECTED:
@@ -100,13 +102,8 @@ public class MainActivity extends AppCompatActivity {
             case CONNECTING:
                 connection.setText("CONNECTING");
                 break;
-            default:
-                break;
         }
-        //I tested several different ways to send information from the connection process side
-        // to other places (such as interfaces, AIDL and singleton ,...) apparently the best way
-        // to send information is broadcast.
-        // So v2ray library will be broadcast information with action V2RAY_CONNECTION_INFO.
+
         v2rayBroadCastReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
@@ -115,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
                     connection_speed.setText("connection speed : " + intent.getExtras().getString(SERVICE_UPLOAD_SPEED_BROADCAST_EXTRA) + " | " + intent.getExtras().getString(SERVICE_DOWNLOAD_SPEED_BROADCAST_EXTRA));
                     connection_traffic.setText("connection traffic : " + intent.getExtras().getString(SERVICE_UPLOAD_TRAFFIC_BROADCAST_EXTRA) + " | " + intent.getExtras().getString(SERVICE_DOWNLOAD_TRAFFIC_BROADCAST_EXTRA));
                     connection_mode.setText("connection mode : " + V2rayConfigs.serviceMode.toString());
-                    switch ((V2rayConstants.CONNECTION_STATES) Objects.requireNonNull(intent.getExtras().getSerializable(SERVICE_CONNECTION_STATE_BROADCAST_EXTRA))) {
+                    switch ((CONNECTION_STATES) Objects.requireNonNull(intent.getExtras().getSerializable(SERVICE_CONNECTION_STATE_BROADCAST_EXTRA))) {
                         case CONNECTED:
                             connection.setText("CONNECTED");
                             break;
@@ -125,8 +122,6 @@ public class MainActivity extends AppCompatActivity {
                             break;
                         case CONNECTING:
                             connection.setText("CONNECTING");
-                            break;
-                        default:
                             break;
                     }
                 });
@@ -140,15 +135,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public static String getDefaultConfig() {
-        return "";
-    }
-
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (v2rayBroadCastReceiver != null){
+        if (v2rayBroadCastReceiver != null) {
             unregisterReceiver(v2rayBroadCastReceiver);
         }
     }
